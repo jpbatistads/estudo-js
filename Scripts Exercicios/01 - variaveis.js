f995c873c4e2327ff927d4c3739f812778f08f19
jpbatista <joao.batista@a.ficr.edu.br>
2020-10-23 13:42:07 -0300

    1° Estruturação de pastas para organizar as informações

---
+++ Scripts Exercicios/01 - variaveis.js
@@ -0,0 +1,18 @@
+/*Declaração de variavel contendo uma string e um number*/
+/*Estas variáveis vão ser usadas no méthodo replace*/
+
+const nome = 'Seu Zé';
+const idade = 24;
+
+
+/*Utilizando variável dentro da string para ser substituida*/
+const mensagemNome = 'Nome de usuário {nome}';/*Utilização {nome}*/
+const mensagemIdade = 'Idade {idade}';/*Utilização {idade}*/
+
+/*Utilizando o methodo de substituição*/
+const mensagemNomeFormatada = mensagemNome.replace('{nome}', nome);
+const mensagemIdadeFormatada = mensagemIdade.replace('{idade}', idade);
+
+const mensagemFinal = mensagemNomeFormatada + ' ' + mensagemIdadeFormatada;
+
+console.log(mensagemFinal);
---
+++ Scripts Exercicios/arryas.js
@@ -0,0 +1,6 @@
+let carrosNovos = [
+    "fiat",
+    "uno",
+    "fusca"
+]
+
