53f69d08922a10d42e5d56818f5b2b91c663e1ca
jpbatista <joao.batista@a.ficr.edu.br>
2020-11-03 23:41:30 -0300

    Adição da pasta pequenos programas

---
+++ Lações De Repetição Scripts/for.js
@@ -0,0 +1,15 @@
+// Array Simples
+const bolsaDeMulher = [
+    "escova",
+    "maquiagem",
+    "perfume",
+    "dinheiro",
+    "óculos",
+    "hidratante"
+]
+
+//O Index = 0 indica o primeiro item do array
+
+for(let index = 0; index < bolsaDeMulher.length; index++){
+    console.log(`${index}. ${bolsaDeMulher[index]}`)
+}
---
+++ concicionais scripts/condicionais.js
@@ -0,0 +1,10 @@
+//Condicionais são blocos de códigos que a partir de uma condição True(verdadeira executam o 1° bloco.)
+//False executam o else.
+
+let velocidade = 150; //Declaração da variável
+
+if (velocidade > 100) {
+  console.log("Você esta acima do limite de velocidade");
+} else {
+  console.log("Parabéns você esta na velocidade normal.");
+}
