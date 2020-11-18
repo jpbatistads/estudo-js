67ff6a57372240608dba5c90fa0f44626031363a
jpbatista <joao.batista@a.ficr.edu.br>
2020-11-18 11:27:40 -0300

    5° Estruturação de pastas para organizar as informações

---
+++ Estrutura de controle scripts/condicionalComposta.js
@@ -0,0 +1,11 @@
+let idade  = 18;
+
+if(idade >= 18){
+    console.log("Você é de maior pode votar.")
+}else if( idade < 18 || idade > 65){
+    console.log("Voto Opcional")
+}else{
+    console.log("Voto obrigátorio")
+}
+
+
