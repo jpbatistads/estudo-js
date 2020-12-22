2781ba49569d0eaa0fdb0a161eddc116dabb2834
jpbatista <joao.batista@a.ficr.edu.br>
2020-12-22 09:34:23 -0300

    Atualização

--- Arrays Scripts/arryas.js
+++ Arrays Scripts/arryas.js
@@ -4,3 +4,18 @@ let carrosNovos = [
     "fusca"
 ]
 
+//Adicionar um dado ao #final de um array = método push
+
+carrosNovos.push("Volvo");
+
+//Remover um dado do #final de um array = método pop
+
+carrosNovos.pop("volvo");
+
+//Adicionar um dado ao #inicio de um array = método unshifit
+
+carrosNovos.unshift("volvo");
+
+//Remover do início do Array método shift
+
+carrosNovos.shift("fiat");
